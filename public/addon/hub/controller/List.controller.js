f755472d6282cadb641b6e356d65511a70d31132
SparkleX <xufei.sun@sap.com>
2021-01-28 15:17:27 +0800

    dev

--- public/addon/hub/controller/List.controller.js
+++ public/addon/hub/controller/List.controller.js
@@ -2,25 +2,31 @@ sap.ui.define(
 	["sap/ui/core/mvc/ControllerExtension", "sap/ui/core/mvc/OverrideExecution"],
 	function (ControllerExtension, OverrideExecution) {
 		"use strict";
+		const addonClazz = sap.ui.requireSync(`partner/AddonExt/controller/List.controller`);
+		const oAddon = new addonClazz();
+		console.debug(oAddon);
+
 		var theClass = ControllerExtension.extend("sap.nsme.crm.AddonExt.controller.List", {
 			metadata: {
 				methods: {}
 			},
 			override: {
 				onInit: function (evt) {
-					alert("Addon:onInit");
+					//alert("Addon:onInit");
 					/*const oController = evt.getSource().getController()
 					oController.onTest = function () {
 						alert("hello");
 					};*/
+					oAddon.getMetadata()._override.onInit(evt);
 				},
 				onClick: function (evt) {
 					var oController = evt.getSource().getParent().getParent().getController();
-					oController.constructor.prototype.onClick();					
+					oController.constructor.prototype.onClick();
 					alert("Addon:onClick");
 				}
 			}
 		});
+
 		return theClass;
 	}
 );
