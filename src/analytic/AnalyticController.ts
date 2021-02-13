6490104d52db7f39a9ac13911f53917818bddaa0
Sun Xufei <xufei.sun@sap.com>
2021-02-13 13:05:47 +0800

    dev

--- src/analytic/AnalyticController.ts
+++ src/analytic/AnalyticController.ts
@@ -4,8 +4,8 @@ import { oAnalyticService } from "./AnalyticService";
 const oAnalyticController = new koaRouter();
 oAnalyticController.prefix("/ana");
 
-oAnalyticController.post("/query", async (ctx) => {
-	ctx.body = await oAnalyticService.query();
+oAnalyticController.post("/query/:id", async (ctx) => {
+	ctx.body = await oAnalyticService.query(ctx.params.id);
 });
 
 export { oAnalyticController };
