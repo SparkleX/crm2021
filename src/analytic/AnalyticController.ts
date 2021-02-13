2694d7d47a5b78c6dc62bbcd791af955d9d06e6d
Sun Xufei <xufei.sun@sap.com>
2021-02-13 14:08:13 +0800

    deb

--- src/analytic/AnalyticController.ts
+++ src/analytic/AnalyticController.ts
@@ -2,7 +2,7 @@ import koaRouter from "koa-router";
 import { oAnalyticService } from "./AnalyticService";
 
 const oAnalyticController = new koaRouter();
-oAnalyticController.prefix("/ana");
+//oAnalyticController.prefix("/");
 
 oAnalyticController.post("/query/:id", async (ctx) => {
 	ctx.body = await oAnalyticService.query(ctx.params.id);
--- src/analytic/AnalyticService.ts
+++ src/analytic/AnalyticService.ts
@@ -2,17 +2,35 @@ import { Connection } from "db-conn";
 import * as jsonfile from "jsonfile";
 import { Context } from "../context/Context";
 import { Chart } from "../gen/schema/Chart";
+import { SemanticLayer } from "../gen/schema/SemanticLayer";
 import { View } from "../gen/schema/View";
 
 export class AnalyticService {
 	async query(chart: string): Promise<any> {
-		const oChart: Chart = jsonfile.readFileSync(`resources/analytics/chart/${chart}.chart.json`);
-		const oSemantic: Chart = jsonfile.readFileSync(`resources/analytics/semantic/${oChart.query}.semantic.json`);
-		const oView: View = jsonfile.readFileSync(`resources/analytics/view/${oSemantic.query}.view.json`);
+		const oChart: Chart = jsonfile.readFileSync(`resources/analytic/chart/${chart}.chart.json`);
+		const oSemantic: SemanticLayer = jsonfile.readFileSync(`resources/analytic/semantic/${oChart.query}.semantic.json`);
+		const oView: View = jsonfile.readFileSync(`resources/analytic/view/${oSemantic.view}.view.json`);
+
+		let sqlMeasure = "";
+		for (const measure in oSemantic.measure) {
+			const oMeasure = oSemantic.measure[measure];
+			sqlMeasure += `${oMeasure.expr} as "${measure}",`;
+		}
+		sqlMeasure = sqlMeasure.substr(0, sqlMeasure.length-1);
+
+		let sqlDim= "";
+		for (const dim in oSemantic.dimension) {
+			//const oDim = oSemantic.dimension[dim];
+			sqlDim += `"${dim}",`;
+		}
+		sqlDim = sqlDim.substr(0, sqlDim.length-1);
+
+		const sql = `select ${sqlDim},${sqlMeasure} from (${oView.sql}) TZ group by ${sqlDim}`;
 		const collection = this.getConnection();
-		const rt = (await collection.executeQuery(`select * from "" where "id"=$1`, [1])) as any;
-		//return rt[0];
-		return oView;
+		const data = await collection.executeQuery(sql);
+		const rt = { data: []};
+		rt.data = data;
+		return rt;
 	}
 	protected getConnection(): Connection {
 		return Context.Current.conn;
